e8a0dcb48ca873ef9822abbdca4814c95d804db6
Allen Spicer - uBack <uBack@stephens-mbp.lan>
2018-04-12 13:54:10 -0400

    update

---
+++ surf/BouySnapshot.swift
@@ -0,0 +1,37 @@
+//
+//  BouySnapshot.swift
+//  surf
+//
+//  Created by uBack on 3/12/18.
+//  Copyright © 2018 surf. All rights reserved.
+//
+
+import Foundation
+
+struct BouySnapshot {
+    let id: String
+    let year: String
+    let month: String
+    let day: String
+    let hour: String
+    let minute: String
+    let windDir: String?
+    let windSpd: String?
+    let gusts: String?
+    let waveHgt: String?
+    let dominantWavePeriod: String?
+    let waveAveragePeriod: String?
+    let meanWaveDirection: String?
+    let PRES: String?
+    let PTDY: String?
+    let airTemp: String?
+    let waterTemp: String?
+    let DEWP: String?
+    let VIS: String?
+    let tide: String?
+//        let lat: String
+//        let lon: String
+
+}
+
+
