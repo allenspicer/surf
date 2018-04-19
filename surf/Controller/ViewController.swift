ca28b973168d40beede0e98704890967cce8faff
Allen Spicer - uBack <allenspicer@gmail.com>
2018-04-19 17:28:59 -0400

    Restructuring

--- surf/Controller/ViewController.swift
+++ surf/Controller/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SnapshotViewController.swift
 //  surf
 //
 //  Created by uBack on 3/4/18.
@@ -14,7 +14,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     private var displayLink: CADisplayLink?
     private var startTime: CFAbsoluteTime?
-    var windUnit = "MPH"
     var path: UIBezierPath!
     var locationManager = CLLocationManager()
     var userLongitude = 0.0
@@ -87,88 +86,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     //TODO: The five functions contained in setUIValuesWithBouyData are all UI. They need to be moved to a view layer
     
     func setUIValuesWithBouyData(){
-        self.addWaveHeightLabels()
+        
+        guard let currentSnapShot  = currentSnapShot else {return}
+        addWaveHeightLabels(currentSnapShot: currentSnapShot, view: self.view)
         self.addSpotTitleLabel()
-        self.addSpotDetails()
         self.view.layer.addSublayer(self.shapeLayer)
         self.startDisplayLink()
     }
     
-    func addWaveHeightLabels(){
-        
-        var waveHeightDigitCount = CGFloat(0)
-        var waveHeight = 0.0
-        if let wHeight = currentSnapShot?.waveHgt as String?{
-            waveHeight = Double(wHeight) ?? 0.0
-        }
-        
-        switch waveHeight{
-        case ...9:
-            waveHeightDigitCount = 2
-        case 10...99:
-            waveHeightDigitCount = 3
-        case 100...:
-            waveHeightDigitCount = 4
-        default:
-            waveHeightDigitCount = 2
-        }
-        let offset: CGFloat = 45 * waveHeightDigitCount
-        
-        let widthPixels = 150 * waveHeightDigitCount + 100
-        
-        let waveHeightLabel = UILabel(frame: CGRect(x: 0, y: 0, width: widthPixels, height: 100))
-        if let waveHeight = currentSnapShot?.waveHgt as String?{
-            waveHeightLabel.text = waveHeight
-        }
-        waveHeightLabel.font = UIFont(name:"Damascus", size: 80.0)
-        waveHeightLabel.textColor =  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        waveHeightLabel.center = CGPoint(x: self.view.frame.width - offset, y: 90)
-        waveHeightLabel.textAlignment = .center
-        view.addSubview(waveHeightLabel)
-        
-        let feetLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-        feetLabel.text = "ft"
-        feetLabel.font = UIFont(name:"Damascus", size: 20.0)
-        feetLabel.textColor =  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        feetLabel.center = CGPoint(x: (self.view.frame.width - offset) + 20 + (waveHeightDigitCount * 20), y: 95)
-        feetLabel.textAlignment = .center
-        view.addSubview(feetLabel)
-    }
-    
-    func addSpotDetails(){
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        if  let speed = currentSnapShot?.windSpd as String?{
-            if let direction = currentSnapShot?.windDir as String?{
-                label.text = speed + " " + windUnit + " " + direction + " WIND"
-            }
-        }
-        label.font = UIFont(name:"Damascus", size: 10.0)
-        label.textColor =  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        let yValue = (self.view.frame.height/5) + 20
-        label.center = CGPoint(x: self.view.frame.width/2, y:yValue)
-        label.textAlignment = .center
-        view.addSubview(label)
-        
-        let waveLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        if let direction = currentSnapShot?.meanWaveDirection as String?{
-            waveLabel.text =  direction + " SWELL"
-        }
-        waveLabel.font = UIFont(name:"Damascus", size: 10.0)
-        waveLabel.textColor =  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        waveLabel.center = CGPoint(x: self.view.frame.width/2, y:yValue + 20)
-        waveLabel.textAlignment = .center
-        view.addSubview(waveLabel)
-        
-        let waterTempLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        if let temp = currentSnapShot?.waterTemp {
-            waterTempLabel.text =  temp + "°F WATER"
-        }
-        waterTempLabel.font = UIFont(name:"Damascus", size: 10.0)
-        waterTempLabel.textColor =  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        waterTempLabel.center = CGPoint(x: self.view.frame.width/2, y: yValue + 40)
-        waterTempLabel.textAlignment = .center
-        view.addSubview(waterTempLabel)
-    }
+
     
     func addSpotTitleLabel(){
         let titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 200))
@@ -182,7 +108,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     
     func addWaveHeightIndicator(){
-        
+    
         let centerY = view.bounds.height / 2
         var waveHeightMaxInt: CGFloat = 0
         if let waveHeight = currentSnapShot?.waveHgt as String?{
@@ -218,7 +144,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 waveIsLabeled = false
             }
         }else{
-            addWaveHeightIndicator()
+//            addWaveHeightIndicator()
             waveIsLabeled = true
         }
     }
---
+++ surf/View/SurfSnapshotView.swift
@@ -0,0 +1,94 @@
+//
+//  SurfSnapshotView.swift
+//  surf
+//
+//  Created by uBack on 4/19/18.
+//  Copyright © 2018 surf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+var windUnit = "MPH"
+
+
+func addWaveHeightLabels(currentSnapShot : Snapshot, view : UIView){
+    
+    var waveHeightDigitCount = CGFloat(0)
+    var waveHeight = 0.0
+    if let wHeight = currentSnapShot.waveHgt as String?{
+        waveHeight = Double(wHeight) ?? 0.0
+    }
+    
+    switch waveHeight{
+    case ...9:
+        waveHeightDigitCount = 2
+    case 10...99:
+        waveHeightDigitCount = 3
+    case 100...:
+        waveHeightDigitCount = 4
+    default:
+        waveHeightDigitCount = 2
+    }
+    let offset: CGFloat = 45 * waveHeightDigitCount
+    
+    let widthPixels = 150 * waveHeightDigitCount + 100
+    
+    let waveHeightLabel = UILabel(frame: CGRect(x: 0, y: 0, width: widthPixels, height: 100))
+    if let waveHeight = currentSnapShot.waveHgt as String?{
+        waveHeightLabel.text = waveHeight
+    }
+    waveHeightLabel.font = UIFont(name:"Damascus", size: 80.0)
+    waveHeightLabel.textColor =  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    waveHeightLabel.center = CGPoint(x: view.frame.width - offset, y: 90)
+    waveHeightLabel.textAlignment = .center
+    view.addSubview(waveHeightLabel)
+    
+    let feetLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+    feetLabel.text = "ft"
+    feetLabel.font = UIFont(name:"Damascus", size: 20.0)
+    feetLabel.textColor =  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    feetLabel.center = CGPoint(x: (view.frame.width - offset) + 20 + (waveHeightDigitCount * 20), y: 95)
+    feetLabel.textAlignment = .center
+    view.addSubview(feetLabel)
+    
+    
+    func addSpotDetails(){
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        if  let speed = currentSnapShot.windSpd as String?{
+            if let direction = currentSnapShot.windDir as String?{
+                label.text = speed + " " + windUnit + " " + direction + " WIND"
+            }
+        }
+        label.font = UIFont(name:"Damascus", size: 10.0)
+        label.textColor =  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        let yValue = (view.frame.height/5) + 20
+        label.center = CGPoint(x: view.frame.width/2, y:yValue)
+        label.textAlignment = .center
+        view.addSubview(label)
+        
+        let waveLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        if let direction = currentSnapShot.meanWaveDirection as String?{
+            waveLabel.text =  direction + " SWELL"
+        }
+        waveLabel.font = UIFont(name:"Damascus", size: 10.0)
+        waveLabel.textColor =  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        waveLabel.center = CGPoint(x: view.frame.width/2, y:yValue + 20)
+        waveLabel.textAlignment = .center
+        view.addSubview(waveLabel)
+        
+        let waterTempLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        if let temp = currentSnapShot.waterTemp {
+            waterTempLabel.text =  temp + "°F WATER"
+        }
+        waterTempLabel.font = UIFont(name:"Damascus", size: 10.0)
+        waterTempLabel.textColor =  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        waterTempLabel.center = CGPoint(x: view.frame.width/2, y: yValue + 40)
+        waterTempLabel.textAlignment = .center
+        view.addSubview(waterTempLabel)
+    }
+}
+
+
