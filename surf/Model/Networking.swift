d4602b68216481e4510159aca7b5925668032ccb
Allen Spicer - uBack <allenspicer@gmail.com>
2018-05-11 10:08:43 -0400

    Creating Network Layer

---
+++ surf/Model/Networking.swift
@@ -0,0 +1,28 @@
+//
+//  Networking.swift
+//  surf
+//
+//  Created by uBack on 5/11/18.
+//  Copyright © 2018 surf. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+func getBouyData (_ stationId: Int) -> String{
+    
+    var dataString = String()
+    
+    do {
+        
+        dataString = try String(contentsOf: URL(string: "http://www.ndbc.noaa.gov/data/realtime2/41110.txt")!)
+    
+    }catch{
+        print("Bouy Data Retreival Error: \(error)")
+    }
+    
+    return dataString
+}
+
