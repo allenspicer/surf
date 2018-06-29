1965b41171f504eb5a1f23a3c0353fde8682bda7
Allen Spicer <allenspicer@gmail.com>
2018-06-28 22:54:52 -0400

    Initial Commit

--- surf/Controller/HomeViewController.swift
+++ surf/Controller/HomeViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import CoreLocation
+import iCarousel
 
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var favoritesCollectionView: UICollectionView!
     @IBOutlet weak var proximalCollectionView: UICollectionView!
+    @IBOutlet weak var carousel: iCarousel!
     private var favoritesData = [Favorite]()
     private var proximalData = [Station]()
     private var cellSelectedIndex = Int()
@@ -35,6 +37,9 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        carousel.type = .rotary
+        carousel.perspective = -0.01
+        
         startActivityIndicator("Loading")
         parseStationList()
         setDataOrGetUserLocation()
@@ -274,7 +279,60 @@ class HomeViewController: UIViewController {
 //MARK: - Extension to handle Collection View and Delegates Assignment
 //
 
-extension HomeViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, CLLocationManagerDelegate, TideClientDelegate, WindClientDelegate, AirTempDelegate, SurfQualityDelegate{
+extension HomeViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, CLLocationManagerDelegate, TideClientDelegate, WindClientDelegate, AirTempDelegate, SurfQualityDelegate, iCarouselDataSource, iCarouselDelegate{
+    
+    
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return favoritesData.count
+    }
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        var label: UILabel
+        var itemView: UIImageView
+        
+        //reuse view if available, otherwise create a new view
+        if let view = view as? UIImageView {
+            itemView = view
+            //get a reference to the label in the recycled view
+            label = itemView.viewWithTag(1) as! UILabel
+        } else {
+            //don't do anything specific to the index within
+            //this `if ... else` statement because the view will be
+            //recycled and used with other index values later
+            itemView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+//            itemView.image = UIImage(named: "page.png")
+            itemView.contentMode = .center
+            itemView.backgroundColor = .black
+            
+            label = UILabel(frame: itemView.bounds)
+            label.backgroundColor = .clear
+            label.textAlignment = .center
+            label.font = label.font.withSize(50)
+            label.tag = 1
+            itemView.addSubview(label)
+        }
+        
+        //set item label
+        //remember to always set any properties of your carousel item
+        //views outside of the `if (view == nil) {...}` check otherwise
+        //you'll get weird issues with carousel item content appearing
+        //in the wrong place in the carousel
+//        label.text = "\(items[index])"
+        
+        return itemView
+    }
+    
+    func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
+        if (option == .spacing) {
+            return value * 1.1
+        }
+        return value
+    }
+    
+
+    
+    
     
     func setDelegatesAndDataSources(){
         favoritesCollectionView.delegate = self
