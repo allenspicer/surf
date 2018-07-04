073670eae2bcfde155156f08b80115552274b73c
Allen Spicer <allenspicer@gmail.com>
2018-07-04 10:45:20 -0400

    Custom Launch/Splash Screen

---
+++ surf/View/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  surf
+//
+//  Created by Allen Spicer on 7/3/18.
+//  Copyright © 2018 surf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    @IBInspectable var firstColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    @IBInspectable var secondColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        get {
+            return CAGradientLayer.self
+        }
+    }
+    
+    func updateView() {
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = [firstColor, secondColor].map{$0.cgColor}
+    }
+}
