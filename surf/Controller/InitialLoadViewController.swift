7534de71d239323d04e8a790776afed18d72a30a
Allen Spicer <allenspicer@gmail.com>
2018-07-05 15:11:36 -0400

    Initial Load View Controller class

---
+++ surf/Controller/InitialLoadViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InitialLoadViewController.swift
+//  surf
+//
+//  Created by Allen Spicer on 7/5/18.
+//  Copyright © 2018 surf. All rights reserved.
+//
+
+import UIKit
+
+class InitialLoadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
