8da29f0e5e120d70364000da04a96ffa2d78b0cf
Allen Spicer <allenspicer@gmail.com>
2018-07-07 17:27:55 -0400

    Initial Load Logic Work

--- surf/Controller/InitialLoadViewController.swift
+++ surf/Controller/InitialLoadViewController.swift
@@ -10,29 +10,21 @@ import UIKit
 
 class InitialLoadViewController: UIViewController {
     
-    var arrayOfSnapshots = [Snapshot]()
+    var arrayOfSnapshots = [Favorite]()
+    var favoritesToBeLoaded = [Favorite]()
     let selectedId = "41110"
     let selectedBFD = 100.0
-    var favoriteSnapshots : [String : Bool]  = ["41110" : false]
+    var favoriteSnapshots : [String : Bool]  = ["101" : false]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         // determine what breaks are in favorites
-        
         DispatchQueue.global(qos:.utility).async{
             self.setUserFavorites(){ (favoritesDictionary) in
                 self.addFavoriteStationsToCollectionData()
             }
         }
-        
-        // load snapshot for each and add to array
-        getSnapshot()
-        
-        //transition when snapshot array is complete and pass it forward
-        
-
     }
     
     func segueWhenComplete(){
@@ -46,37 +38,40 @@ class InitialLoadViewController: UIViewController {
         DispatchQueue.global(qos:.utility).async {
             let snapshotSetter = SnapshotSetter(stationId: self.selectedId, beachFaceDirection: self.selectedBFD)
             let snapshot = snapshotSetter.createSnapshot(finished: {})
-            //remove spinner for response:
+            //if snapshot worked update snapshot array
             if snapshot.waveHgt != nil && snapshot.waterTemp != nil {
                 self.favoriteSnapshots["\(self.selectedId)"] = true
+                //segue when all snapshots are available
                 self.segueWhenComplete()
-            }else{
-                DispatchQueue.main.async {
-                    //if no data respond with alertview
-                    let alert = UIAlertController.init(title: "Not enough Data", message: "This bouy is not providing much data at the moment", preferredStyle: .alert)
-                    let doneAction = UIAlertAction(title: "Cancel", style: .destructive)
-                    alert.addAction(doneAction)
-                    self.present(alert, animated: true, completion: nil)
+//            }else{
+                
+                //delay then try creating endpoint again
+                //eventually show user error. 
+                
+                
+//                DispatchQueue.main.async {
+//                    //if no data respond with alertview
+//                    let alert = UIAlertController.init(title: "Not enough Data", message: "This bouy is not providing much data at the moment", preferredStyle: .alert)
+//                    let doneAction = UIAlertAction(title: "Cancel", style: .destructive)
+//                    alert.addAction(doneAction)
+//                    self.present(alert, animated: true, completion: nil)
                 }
             }
         }
     }
     
     func setUserFavorites (completion:@escaping ([String : Int])->Void){
+        
+        // retrieve defaults and set up a local dictionary with the users favorites and a false flag
+        // false here is a default value to indicate this favorite has not been downloaded
+        
         let defaults = UserDefaults.standard
         if let favorites = defaults.array(forKey: DefaultConstants.favorites) as? [Int], let names = defaults.array(forKey: DefaultConstants.nicknames) as? [String]{
-            
-            // here the local id is available
-            // we need to use the id to get the station id, then append to favoritesnapshots array
-            // then use getsnapshot to get all data
-            
-            
-//            favoritesArray = favorites
-//            nicknamesArray = names
             for index in 0..<favorites.count {
                 let favorite = favorites[index]
-                let name = names[index]
-//                favoriteStationIdsFromMemory[name] = favorite
+                // currently doing nothing with the nickname chosen by the user and saved in defaults
+//                let name = names[index]
+                    favoriteSnapshots["\(favorite)"] = false
             }
         }
         completion([String : Int]())
@@ -95,20 +90,19 @@ class InitialLoadViewController: UIViewController {
                 if let metaData = jsonResult as? [[String : AnyObject]]{
                     for station in metaData {
                         guard let id = station["id"] as? Int else {return}
-//                        if !self.favoriteStationIdsFromMemory.values.contains(id) {continue}
+                        if self.favoriteSnapshots.keys.contains("\(id)") {continue}
                         guard let stationId = station["station"] as? Int else {return}
                         guard let beachFaceDirection = station["bfd"] as? Double else {return}
                         guard let name = station["name"] as? String else {return}
                         let favorite = Favorite(id: "\(id)", stationId: "\(stationId)", beachFaceDirection: beachFaceDirection, name: name)
-//                        favoritesData.append(favorite)
+                        arrayOfSnapshots.append(favorite)
                     }
-                    DispatchQueue.main.async{
-//                        self.carousel.type = .rotary
-//                        self.carousel.perspective = -0.0020
-//                        self.carousel.viewpointOffset = CGSize(width: 0, height: -125)
-//                        self.carousel.dataSource = self
-//                        self.carousel.delegate = self
-//                        self.stopActivityIndicator()
+                        // load snapshot for each Favorite and add to array
+                        for id in favoriteSnapshots.keys {
+                            self.getSnapshot()
+                            //use favorite.stationid for snapshot
+                            //append to snapshot array
+
                     }
                 }
             } catch {
@@ -123,7 +117,7 @@ class InitialLoadViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if let destinationVC = segue.destination as? HomeViewController {
-            destinationVC.arrayOfSnapshots = arrayOfSnapshots
+            destinationVC.favoritesFromMemory = arrayOfSnapshots
         }
     }
  
