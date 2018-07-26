61c801d261d21918c0cdcb7c9de9d448f63af424
Allen Spicer <allenspicer@gmail.com>
2018-07-26 16:05:39 -0400

    Empty Project. File structure created. Initial VC created.

---
+++ surfbreak/surfbreak/Controllers/InitialViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  surfbreak
+//
+//  Created by Allen Spicer on 7/26/18.
+//  Copyright © 2018 surfbreak. All rights reserved.
+//
+
+import UIKit
+
+final class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        //set up activity indicator
+        //trigger user location process
+        //check persistence for user favorites
+        //for each favorite
+            // load series of data points (clients)
+
+    }
+
+
+
+}
+
+extension InitialViewController {
+    
+}
+
+extension InitialViewController {
+    
+}
+
+extension InitialViewController {
+    
+}
+
+extension InitialViewController {
+    
+}
+
