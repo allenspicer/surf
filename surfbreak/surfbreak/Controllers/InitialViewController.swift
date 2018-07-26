f2ab2988149dc9c3cf71ba683bc4c43553943985
Allen Spicer <allenspicer@gmail.com>
2018-07-26 18:00:44 -0400

    Persistence entity for user location. Multithreading on utility for background work.

--- surfbreak/surfbreak/Controllers/InitialViewController.swift
+++ surfbreak/surfbreak/Controllers/InitialViewController.swift
@@ -13,19 +13,34 @@ final class InitialViewController: UIViewController {
     
     private var locationManager = CLLocationManager()
     private var userLocation = (0.0,0.0)
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
 
     override func viewDidLoad() {
         //set up activity indicator
+        DispatchQueue.global(qos:.utility).async{
+
         //trigger user location process
-        getUserLocation()
+            self.getUserLocation()
         //check persistence for user favorites
         //for each favorite
             // load series of data points (clients)
-
+        }
     }
 
     private func getUserLocation (){
+        var locationArray = [UserLocation]()
+        do {
+            locationArray = try context.fetch(UserLocation.fetchRequest())
+        }
+        catch {
+            print("Failed to retrieve UserLocation Entity from context.")
+        }
+        for location in locationArray {
+            userLocation.0 = location.latitude
+            userLocation.1 = location.longitude
+        }
+
         if userLocation.0 != 0.0 && userLocation.1 != 0.0 {
             //user location available
             //apply location to find distances
@@ -54,9 +69,9 @@ extension InitialViewController : CLLocationManagerDelegate{
     }
     
     
-    //this method will be called each time when a user change his location access preference.
+    //this method will be called each time a user changes his location access preference.
     internal func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if status == .authorizedWhenInUse {
+        if status == .authorizedWhenInUse || status == .authorizedAlways {
             print("User allowed us to access location")
             locationManager.requestLocation();
         }
@@ -65,13 +80,12 @@ extension InitialViewController : CLLocationManagerDelegate{
     
     //this method is called by the framework on         locationManager.requestLocation();
     internal func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print("Did location updates is called")
-        print(locations)
+        print("Did location updates called")
         setLocationDataFromResponse()
     }
     
     internal func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Did location updates is called but failed getting location \(error)")
+        print("Did location updates called but failed getting location \(error)")
     }
     
     private func setLocationDataFromResponse(){
@@ -79,6 +93,16 @@ extension InitialViewController : CLLocationManagerDelegate{
             userLocation.0 = currentLocation.coordinate.latitude
             userLocation.1 = currentLocation.coordinate.longitude
             print(userLocation)
+            
+            //save user location to persistence
+            DispatchQueue.main.async {
+                //add to persistent container
+                let location = UserLocation(context: self.context)
+                location.timestamp = Date()
+                location.latitude = self.userLocation.0
+                location.longitude = self.userLocation.1
+                (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            }
         }
     }
 }
