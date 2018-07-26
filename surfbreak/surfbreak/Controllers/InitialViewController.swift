03f5ebfca18b8a131c4540bd64288933bb0cf5f6
Allen Spicer <allenspicer@gmail.com>
2018-07-26 16:31:35 -0400

    User Location retrieved in initial view controller

--- surfbreak/surfbreak/Controllers/InitialViewController.swift
+++ surfbreak/surfbreak/Controllers/InitialViewController.swift
@@ -7,24 +7,80 @@
 //
 
 import UIKit
+import CoreLocation
 
 final class InitialViewController: UIViewController {
+    
+    private var locationManager = CLLocationManager()
+    private var userLocation = (0.0,0.0)
+
 
     override func viewDidLoad() {
         //set up activity indicator
         //trigger user location process
+        getUserLocation()
         //check persistence for user favorites
         //for each favorite
             // load series of data points (clients)
 
     }
 
-
+    private func getUserLocation (){
+        if userLocation.0 != 0.0 && userLocation.1 != 0.0 {
+            //user location available
+            //apply location to find distances
+        }else{
+            isAuthorizedtoGetUserLocation()
+            
+            if CLLocationManager.locationServicesEnabled() {
+                locationManager.delegate = self
+                locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            }
+            
+            if CLLocationManager.locationServicesEnabled() {
+                locationManager.requestLocation();
+            }
+        }
+    }
 
 }
 
-extension InitialViewController {
+extension InitialViewController : CLLocationManagerDelegate{
+    //if we have no permission to access user location, then ask user for permission.
+    private func isAuthorizedtoGetUserLocation() {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse     {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
     
+    
+    //this method will be called each time when a user change his location access preference.
+    internal func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            print("User allowed us to access location")
+            locationManager.requestLocation();
+        }
+    }
+    
+    
+    //this method is called by the framework on         locationManager.requestLocation();
+    internal func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Did location updates is called")
+        print(locations)
+        setLocationDataFromResponse()
+    }
+    
+    internal func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Did location updates is called but failed getting location \(error)")
+    }
+    
+    private func setLocationDataFromResponse(){
+        if  let currentLocation = locationManager.location{
+            userLocation.0 = currentLocation.coordinate.latitude
+            userLocation.1 = currentLocation.coordinate.longitude
+            print(userLocation)
+        }
+    }
 }
 
 extension InitialViewController {
