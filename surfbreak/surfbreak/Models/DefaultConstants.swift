9a026a17a1b62f5f40e3b6eda1104ea82a89a5ae
Allen Spicer <allenspicer@gmail.com>
2018-08-02 21:27:47 -0400

    Added controller for detail and constants struct for defaults

---
+++ surfbreak/surfbreak/Models/DefaultConstants.swift
@@ -0,0 +1,15 @@
+//
+//  DefaultConstants.swift
+//  surf
+//
+//  Created by Allen Spicer on 7/4/18.
+//  Copyright © 2018 surf. All rights reserved.
+//
+
+import Foundation
+
+
+struct DefaultConstants {
+    static let favorites = "favorites"
+    static let nicknames = "nicknames"
+}
