0b851db072755cae04846913fef53d6937b5ab75
Allen Spicer <allenspicer@gmail.com>
2018-10-15 17:33:59 -0400

    Clean up ignored files

--- surfbreak/Pods/Target Support Files/Pods-surfbreak/Pods-surfbreak-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_surfbreak : NSObject
-@end
-@implementation PodsDummy_Pods_surfbreak
-@end
--- surfbreak/Pods/Target Support Files/Pods-surfbreakTests/Pods-surfbreakTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_surfbreakTests : NSObject
-@end
-@implementation PodsDummy_Pods_surfbreakTests
-@end
