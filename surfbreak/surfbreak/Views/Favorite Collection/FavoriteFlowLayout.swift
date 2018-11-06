241f18269b39539f061a317f0d359afa3c6fa5ad
Allen Spicer <allenspicer@gmail.com>
2018-11-06 12:04:40 -0500

    Flow layout formatting update. Removed unused code.

--- surfbreak/surfbreak/Views/Favorite Collection/FavoriteFlowLayout.swift
+++ surfbreak/surfbreak/Views/Favorite Collection/FavoriteFlowLayout.swift
@@ -13,7 +13,7 @@ class FavoriteFlowLayout: UICollectionViewFlowLayout {
     var standardItemScale: CGFloat = 0.65
     var standardVerticalOffset: CGFloat = 100.0
     var screenHeightOffset: CGFloat =  0.6 * (UIScreen.main.bounds.size.height - 812)
-
+    
     var isSetup = false
     
     override func prepare() {
@@ -57,35 +57,24 @@ class FavoriteFlowLayout: UICollectionViewFlowLayout {
         attributes.transform3D = CATransform3DScale(CATransform3DIdentity, scale, scale, 1)
         attributes.zIndex = Int(alpha * 10)
         attributes.center = CGPoint(x: attributes.center.x, y: (attributes.center.y + (standardVerticalOffset * ratio) + screenHeightOffset))
-        
     }
     
     
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-        
         let layoutAttributes = self.layoutAttributesForElements(in: collectionView!.bounds)
-        
         let center = collectionView!.bounds.size.width / 2
         let proposedContentOffsetCenterOrigin = proposedContentOffset.x + center
-        
         let closest = layoutAttributes!.sorted { abs($0.center.x - proposedContentOffsetCenterOrigin) < abs($1.center.x - proposedContentOffsetCenterOrigin) }.first ?? UICollectionViewLayoutAttributes()
-        
         let targetContentOffset = CGPoint(x: floor(closest.center.x - center), y: proposedContentOffset.y)
-
         return targetContentOffset
     }
     
     
     func setupCollectionView() {
         self.collectionView!.decelerationRate = UIScrollViewDecelerationRateFast
-        
         let collectionSize = collectionView!.bounds.size
-//        let yInset = (collectionSize.height - self.itemSize.height) / 2
         let xInset = (collectionSize.width - self.itemSize.width) / 2
-//
-//        self.sectionInset = UIEdgeInsetsMake(yInset, xInset, yInset, xInset)
         self.sectionInset = UIEdgeInsetsMake(0, xInset, 0, xInset)
-
     }
     
 }
