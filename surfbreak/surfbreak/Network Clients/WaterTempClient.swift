0cbf44e97d80213e17d04a748286c37119ad68a6
Allen Spicer <allenspicer@gmail.com>
2018-11-09 14:27:38 -0500

    Complete separation of buoy and water temp clients

--- surfbreak/surfbreak/Network Clients/WaterTempClient.swift
+++ surfbreak/surfbreak/Network Clients/WaterTempClient.swift
@@ -38,46 +38,7 @@ final class WaterTempClient: NSObject {
     //
     
     
-    private func buoyDataServiceRequestWith(waveurl : URL, waterTempurl : URL){
-        
-        var dataString = String()
-        do {
-            dataString = try String(contentsOf: waveurl)
-        }catch{
-            print("Wave Bouy Data Retreival Error: \(error)")
-            DispatchQueue.main.async {
-                self.delegate?.didFinishBuoyTask(sender: self, snapshot: self.currentSnapshot, stations: self.allStations)
-            }
-            return
-        }
-        
-        var lines = dataString.components(separatedBy: "\n")
-        var values = lines[1].components(separatedBy: ",")
-
-        //wave height
-        guard let currentWaveHeight = Double(values[5]) as Double? else {return}
-        let formatter = NumberFormatter()
-        formatter.maximumFractionDigits = 1
-        var heightInFeet = currentWaveHeight * 3.28
-        heightInFeet = (heightInFeet*10).rounded()/10
-
-        //wave direction
-        guard let waveToDirectionDegrees = Double(values[13]) as Double? else {return}
-        let waveToDirectionInt = Int(waveToDirectionDegrees)
-        let waveFromDirectionInt = abs(360 - waveToDirectionInt)
-
-        //wave frequency/period
-        guard let waveAveragePeriod = Double(values[7]) as Double? else {return}
-
-        currentSnapshot.waveHeight = heightInFeet
-        currentSnapshot.swellDirection = waveFromDirectionInt
-//        currentSnapshot.swellDirectionString = directionFromDegrees(degrees: Float(waveFromDirectionInt))
-        currentSnapshot.period = waveAveragePeriod
-        currentSnapshot.beachFaceDirection = currentStation.bfd
-        currentSnapshot.id = currentStation.id
-        currentSnapshot.stationId = currentStation.station
-        currentSnapshot.stationName = currentStation.name
-        currentSnapshot.airWindTideId = currentStation.airWindTideId
+    private func waterTempDataServiceRequestWith(waterTempurl : URL){
         
         var waterTempDataString = String()
         do {
@@ -111,18 +72,16 @@ final class WaterTempClient: NSObject {
         
         for station in allStations where station.id == self.snapshotId {
             currentStation = station
-            let urlString = "https://sdf.ndbc.noaa.gov/sos/server.php?request=GetObservation&service=SOS&version=1.0.0&offering=urn:ioos:station:wmo:\(currentStation.station)&observedproperty=Waves&responseformat=text/csv&eventtime=latest"
-            guard let waveurl = URL(string: urlString) else {return}
             
             let waterTempurlString = "https://sdf.ndbc.noaa.gov/sos/server.php?request=GetObservation&service=SOS&version=1.0.0&offering=urn:ioos:station:wmo:\(currentStation.station)&observedproperty=sea_water_temperature&responseformat=text/csv&eventtime=latest"
             guard let waterTempurl = URL(string: waterTempurlString) else {return}
             
-            buoyDataServiceRequestWith(waveurl: waveurl, waterTempurl: waterTempurl)
+            waterTempDataServiceRequestWith(waterTempurl: waterTempurl)
         }
     }
 }
 
-extension BuoyClient {
+extension WaterTempClient {
     
     //
     //MARK: - helpers to convert data
